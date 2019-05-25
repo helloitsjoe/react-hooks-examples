df01483a0336f66956a25627eb8b916959826127
helloitsjoe <jboyle617@gmail.com>
2019-05-24 20:33:09 -0400

    docs/ -> build/

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Counter from './01-Counter';
 import Clock from './02-Clock';
 import Vacation from './03-Vacation';
 import './App.css';
 
-function Button({ app, current, setCurrent }) {
+const AppContext = React.createContext();
+
+function Button({ app }) {
+  const { current, setCurrent } = useContext(AppContext);
   return (
     <button
       className="global-button"
@@ -13,18 +16,20 @@ function Button({ app, current, setCurrent }) {
     >
       {app.name}
     </button>
-  )
+  );
 }
 
 export default function App() {
   const [current, setCurrent] = useState({ app: Counter });
 
   return (
-    <div className="App">
-      <Button app={Counter} setCurrent={setCurrent} current={current} /> 
-      <Button app={Clock} setCurrent={setCurrent} current={current} /> 
-      <Button app={Vacation} setCurrent={setCurrent} current={current} /> 
-      <current.app />
-    </div>
+    <AppContext.Provider value={{ current, setCurrent }}>
+      <div className="App">
+        <Button app={Counter} />
+        <Button app={Clock} />
+        <Button app={Vacation} />
+        <current.app />
+      </div>
+    </AppContext.Provider>
   );
 }
