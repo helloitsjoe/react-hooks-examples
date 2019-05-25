45281cb0b1605082c18e0d3e5ec1e331f2789ae8
helloitsjoe <jboyle617@gmail.com>
2019-05-24 20:33:16 -0400

    docs/ -> build/

---
+++ build/precache-manifest.4ad04ae5776e93b4be1e646596e1d307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565de9fcdc713739747e7186d8a225eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d21a1f3523b6a54ae03",
+    "url": "/static/css/main.b036de94.chunk.css"
+  },
+  {
+    "revision": "c90412ba0399d04dfed3",
+    "url": "/static/js/2.08b9a561.chunk.js"
+  },
+  {
+    "revision": "4d21a1f3523b6a54ae03",
+    "url": "/static/js/main.10c96ebe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
