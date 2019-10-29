07d6ecfc6afe1d023adb6eeee00ed6709c13134e
Joe Boyle <joboyle@wayfair.com>
2019-10-29 17:19:38 -0400

    remove Class implementations

--- src/01-Counter.js
+++ src/01-Counter.js
@@ -55,28 +55,3 @@ Counter.displayName = 'Counter';
 // }
 
 // render();
-
-////////////////////////////////////////////
-// CLASS-BASED IMPLEMENTATION
-
-// export default class Counter extends React.Component {
-//   static displayName = 'Counter';
-
-//   state = {
-//     count: 0,
-//   };
-
-//   increment = () => this.setState(prev => ({ count: prev.count + 1 }));
-//   decrement = () => this.setState(prev => ({ count: prev.count - 1 }));
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Current count:</h1>
-//         <h1>{this.state.count}</h1>
-//         <button onClick={this.increment}>+</button>
-//         <button onClick={this.decrement}>-</button>
-//       </div>
-//     );
-//   }
-// }
--- src/03-Vacation.js
+++ src/03-Vacation.js
@@ -42,84 +42,17 @@ export default function Vacation() {
 
 Vacation.displayName = 'Vacation';
 
-// CLASS-BASED IMPLEMENTATION
-
-// export default class Vacation extends React.Component {
-//   static displayName = 'Vacation';
-
-//   state = {
-//     loading: false,
-//     error: false,
-//     imageData: null,
-//     input: '',
-//     query: 'Rome',
-//   };
-
-//   componentDidMount() {
-//     this.fetch(this.state.query);
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.query !== prevState.query) {
-//       this.fetch(this.state.query);
-//     }
-//   }
-
-//   fetch = query => {
-//     this.setState({ loading: true, error: false });
-//     fetchImage(query)
-//       .then(data => {
-//         this.setState({
-//           loading: false,
-//           imageData: getRandom(data.results),
-//         });
-//       })
-//       .catch(err => {
-//         this.setState({ loading: false, error: true });
-//       });
-//   };
-
-//   handleChange = e => this.setState({ input: e.target.value });
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.setState(prev => ({ query: prev.input }));
-//   };
-
-//   render() {
-//     const { loading, error, imageData, query, input } = this.state;
-//     const { alt, src, hotlink } = getImgAttrs(imageData);
-
-//     if (loading || error) return <Fallback error={error} />;
-
-//     return (
-//       <div className="App">
-//         <form onSubmit={this.handleSubmit}>
-//           {query && <h1>{query}!</h1>}
-//           {src ? (
-//             <a href={hotlink}>
-//               <img alt={alt} src={src} />
-//             </a>
-//           ) : (
-//             <h2>Where would you like to go?</h2>
-//           )}
-//           <input
-//             className="Vacation-input"
-//             placeholder="Where should we go next?"
-//             onChange={this.handleChange}
-//             value={input}
-//           />
-//           <button type="submit">Search</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
 const Fallback = ({ loading, error }) => {
   return (
     <div data-testid="fallback" className="App">
-      {error ? <h1 style={{ color: 'red' }}>Error! {error}</h1> : <h1>Loading...</h1>}
+      {error ? (
+        <div className="error">
+          <h1>Error!</h1>
+          <p>See devtools for more information</p>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 };
