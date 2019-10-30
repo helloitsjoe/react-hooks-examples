c912b48aacda0edf390c7c43542d8d17b5b09196
helloitsjoe <jboyle617@gmail.com>
2019-10-29 20:18:39 -0400

    remove ShallowTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,3 @@
-import { componentSpy } from './__tests__/test-utils';
-
-global.componentSpy = componentSpy;
-
 const silenceActWarnings = log => (message, ...args) => {
   if (/act(...)/.test(message)) return;
   return log(message, ...args);
